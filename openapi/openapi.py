a6676a653a68d15f5e13a8d86baa86ac3d505df0
wangjie <wangjie@htffund.com>
2023-05-05 19:20:41 +0800

    feature: add csp_openapi.yaml

--- openapi/openapi.py
+++ openapi/openapi.py
@@ -1,9 +1,15 @@
 import os, yaml
 from langchain.llms.openai import OpenAI
 from langchain.requests import RequestsWrapper
+import openai
+
+openai.proxy = {
+    "http":"http://127.0.0.1:12334",
+    "https":"http://127.0.0.1:12334"
+}
 
 from langchain.agents.agent_toolkits.openapi.spec import reduce_openapi_spec
-os.environ['OPENAI_API_KEY'] = 'your openai key'
+os.environ['OPENAI_API_KEY'] = 'your openai token'
 
 from langchain.agents.agent_toolkits.openapi import planner
 
@@ -19,6 +25,10 @@ with open("spotify_openapi.yaml", encoding='utf-8') as f:
     raw_spotify_api_spec = yaml.load(f, Loader=yaml.Loader)
 spotify_api_spec = reduce_openapi_spec(raw_spotify_api_spec)
 
+with open("csp_openapi.yaml", encoding='utf-8') as f:
+    raw_csp_api_spec = yaml.load(f, Loader=yaml.Loader)
+csp_api_spec = reduce_openapi_spec(raw_csp_api_spec)
+
 
 headers = {
     "Authorization": f"Bearer {os.getenv('OPENAI_API_KEY')}"
@@ -27,6 +37,6 @@ headers = {
 llm = OpenAI(model_name="text-davinci-003", temperature=0.0)
 
 openai_requests_wrapper = RequestsWrapper(headers=headers)
-openai_agent = planner.create_openapi_agent(openai_api_spec, openai_requests_wrapper, llm)
-user_query = "generate a short piece of advice"
+openai_agent = planner.create_openapi_agent(csp_api_spec, openai_requests_wrapper, llm)
+user_query = "基金000330的赎回交易是哪天交收的"
 openai_agent.run(user_query)
